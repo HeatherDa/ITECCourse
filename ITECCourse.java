d47f76d2bb65a5377e62389d95401fa28bf8f49b
HeatherDa <crystal_tree@hotmail.com>
2016-03-02 18:25:52 -0600

    Added new course and changed Access Modifiers

--- ITECCourse.java
+++ ITECCourse.java
@@ -5,20 +5,22 @@ import java.util.*;
  */
 public class ITECCourse {
     //Data for an ITECCourse object to store
-    String name;
-    int code;
-    String room;
-    ArrayList<String> students;
-    int maxStudents;
+    private String name;
+    private int code;
+    private String room;
+    private ArrayList<String> students;
+    private int maxStudents;
+
+
 
     //Constructor
-        ITECCourse(String courseName, int courseCode, String courseRoom, int courseMaxStudents) {
-            this.name = courseName;
-            this.code = courseCode;
-            this.room=courseRoom;
-            this.students = new ArrayList<String>();
-            this.maxStudents = courseMaxStudents;
-        }
+    ITECCourse(String courseName, int courseCode, String courseRoom, int courseMaxStudents) {
+        this.name = courseName;
+        this.code = courseCode;
+        this.room = courseRoom;
+        this.students = new ArrayList<String>();
+        this.maxStudents = courseMaxStudents;
+    }
 
     void addStudent(String studentName) {
         //if the number of students is the same as the max allowed, can't add any more.
@@ -27,7 +29,7 @@ public class ITECCourse {
             System.out.println(studentName+" was added to "+this.name);
         }else {
             //there is no room for this student
-            System.out.println(this.name+" is full, max number of students is "+this.maxStudents+".  "+studentName+" was not added.");
+            System.out.println(this.name+" is full, max number of students is "+this.maxStudents+".  "+studentName+" was not added.\n");
         }
     }
     void removeStudent(String studentName){
@@ -38,21 +40,36 @@ public class ITECCourse {
             System.out.println(studentName+" was not found in this course.");
         }
     }
+    public String getCourseName(){
+        return this.name;
+    }
+
+    public void setCourseName(String newName){
+        this.name=newName;
+    }
+
+    public String getRoom(){
+        return this.room;
+    }
+
+    public void setRoom(String newRoom){
+        this.room=newRoom;
+    }
+
+    public int getNumberOfStudents() {
+        return this.students.size();
+    }
 
     public void writeCourseInfo() {
-        System.out.println("Course Name: " + this.name);
+        System.out.println("\nCourse Name: " + this.name);
         System.out.println("Course Code: " + this.code);
         System.out.println("Room Number: " + this.room);
         System.out.println("Students enrolled:");
         for (String student : this.students) {
             System.out.println(student);
         }
-        System.out.println("There are " + getNumberOfStudents() + " students enrolled");
-        System.out.println("The max number of students that can enroll in this course is " + this.maxStudents);
-    }
-
-    public int getNumberOfStudents() {
-        return this.students.size();
+        System.out.println("There are " + getNumberOfStudents() + " students enrolled.");
+        System.out.println("The max number of students that can enroll in this course is " + this.maxStudents+".\n");
     }
 }
 
--- ITECCourseManager.java
+++ ITECCourseManager.java
@@ -3,8 +3,7 @@ package com.Heather;
 public class ITECCourseManager {
 
     public static void main(String args[]) {
-        ITECCourse maintenanceCourse =
-            new ITECCourse("Microcomputer Systems Maintenance", 1310, "T3020", 20);
+        ITECCourse maintenanceCourse = new ITECCourse("Microcomputer Systems Maintenance", 1310, "T3020", 20);
         //Add some students
         maintenanceCourse.addStudent("Anna");
         maintenanceCourse.addStudent("Bill");
@@ -26,11 +25,23 @@ public class ITECCourseManager {
 
         //Test the add students method with a sample class//This class has a max of 3 students
         ITECCourse smallCourse = new ITECCourse("Small Course", 1111, "T3022", 3);
+        System.out.println("This course is called "+smallCourse.getCourseName());
         smallCourse.addStudent("Jake");
         smallCourse.addStudent("Kirby");
         smallCourse.addStudent("Liam");
         //We can't add this student – what happens?
         smallCourse.addStudent("Marigold");
+        smallCourse.setCourseName("Very Small Course");
+        smallCourse.writeCourseInfo();
+        //Another course
+        ITECCourse infoCourse=new ITECCourse("Info Tech Concepts", 1100, "T3050", 30);
+        infoCourse.addStudent("Max");
+        infoCourse.addStudent("Nancy");
+        infoCourse.addStudent("Orson");
+        System.out.println("The room number for this course is usually "+infoCourse.getRoom());
+        infoCourse.setRoom("T3010");
+        System.out.println("The room number for "+infoCourse.getCourseName()+" has been changed to "+infoCourse.getRoom()+".\n");
+        infoCourse.writeCourseInfo();
     }
 }
 
